9c1a3e940344744cd0070a188f1e29d648246875
Konstantin Krivopustov <krivopustov@haulmont.com>
2019-03-13 16:04:58 +0400

    Legacy browser

---
+++ modules/web/src/com/haulmont/teamprogress/web/weekreport_legacy/WeekReportLegacyBrowse.java
@@ -0,0 +1,6 @@
+package com.haulmont.teamprogress.web.weekreport_legacy;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class WeekReportLegacyBrowse extends AbstractLookup {
+}
